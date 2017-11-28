6a4e9492e9302c7dce8a3003743431acd0afb177
qianli <qianli_xy@163.com>
2017-11-28 22:49:43 +0800

    调整放大缩小

--- src/static/js/jquery.zoom.js
+++ src/static/js/jquery.zoom.js
@@ -1,24 +1,35 @@
 ;(function($) {
     $.fn.zoom = function(settings) {
-        var opts = $.extend({scale:0, speed:300},
-            ("object" === typeof settings) ? settings :
-            {scale : parseFloat(settings)});
+        var opts = $.extend({scale:0, speed:300}, ("object" === typeof settings) ? settings : {scale : parseFloat(settings)});
         return this.each(function() {
-            var $this = $(this),
-                top = $this.intCss("top"),
-                left = $this.intCss("left"),
-                width = $this.intCss("width"),
-                height = $this.intCss("height"),
-                ratio = height / width,
-                incr = opts.scale * opts.speed
+            var $this = $(this);
+
+            if(!$this.data("width")) {
+                var width = $this.intCss("width"),
+                    height = $this.intCss("height"),
+                    ratio = height / width;
+                $this.data("width", width)
+                    .data("height", height)
+                    .data("ratio", height / width)
+                    .data("top", $this.intCss("top"))
+                    .data("left", $this.intCss("left"))
+                    .data("scale", opts.scale);
+            } else {
+                // if($this.intCss("width") <= 0 && opts.scale < 0) {
+                //     return;
+                // }
+                $this.data("scale", $this.data("scale") + opts.scale);
+            }
+
+            var incr = $this.data("scale") * opts.speed;
 
             $this.css({
                 position: "absolute",
-                width: width + incr,
+                width: $this.data("width") + incr,
                 maxWidth: "none",
                 maxHeight: "none",
-                top: top - incr / 2,
-                left: left - incr / 2 * ratio
+                top: $this.data("top") - parseInt(incr / 2 * $this.data("ratio")),
+                left: $this.data("left") - incr / 2
             });
         });
     }
